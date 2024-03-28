6789336f559086f70dd072d018aebf4f4c026d0d
Nui Suriya <atchara.suriya@gmail.com>
2024-03-28 00:52:11 +0000

    stage 1: print item list and price

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+  public static void main(String[] args) {
+    // Step 1: Display item list and price
+    System.out.println("Prices:");
+    System.out.println("Bubblegum: " + "$" + 2);
+    System.out.println("Toffee: " + "$" + 0.2);
+    System.out.println("Ice cream: " + "$" + 5);
+    System.out.println("Milk chocolate: " + "$" + 4);
+    System.out.println("Doughnut: " + "$" + 2.5);
+    System.out.println("Pancake: " + "$" + 3.2);
+  }
+}
